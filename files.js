1144ff7ca4dd11c6ce40a902d4a80c5a7fb46c2f
Voimaaa <104168513+Voimaaa@users.noreply.github.com>
2026-02-19 14:30:00 +0100

    Add files via upload

---
+++ files.js
@@ -0,0 +1,26 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+const args = process.argv.slice(2);
+if (args.length === 0) {
+  console.error("Fehler: Kein Name für den Ordner übergeben!");
+  process.exit(1);
+}
+
+const folderName = args[0] + "_storage";
+
+const volumePath = "/storage/"; 
+const fullPath = path.join(volumePath, folderName);
+
+try {
+  if (!fs.existsSync(volumePath)) {
+    console.error(`Das Volume unter ${volumePath} wurde nicht gefunden!`);
+  } else if (!fs.existsSync(fullPath)) {
+    fs.mkdirSync(fullPath);
+    console.log(`Ordner "${folderName}" wurde erfolgreich im Volume erstellt`);
+  } else {
+    console.log(`Ordner "${folderName}" existiert bereits im Volume`);
+  }
+} catch (err) {
+  console.error("Fehler beim Erstellen:", err.message);
+}
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const app = require('express')();
+const { exec } = require('child_process');
 const PORT = 8080;
 
 app.get('/api/daten', (req, res) => {
-    const empfangenerWert = req.query.meinParameter;
-    console.log("Der Client hat geschickt:", empfangenerWert);
-    res.json({ status: "erhalten", deinWert: empfangenerWert });
+    const email = req.query.meinParameter;
+    
+    exec(`node files.js ${email}`, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Fehler: ${error.message}`);
+          return;
+        }
+        console.log(`Ausgabe: ${stdout}`);
+      });
 });
 
 app.listen(
